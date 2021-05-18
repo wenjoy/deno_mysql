985661e80f4cb04c95b4e9602d424cc2709054f3
George Xie <georgexie@George.local>
2021-05-18 22:06:22 +0800

    fix: add notes

--- src/auth_plugin/crypt.ts
+++ src/auth_plugin/crypt.ts
@@ -3,8 +3,12 @@ const forge = (self as any).forge;
 const { publicKeyFromPem } = forge.pki;
 function encryptWithPublicKey(key: string, data: Uint8Array):Uint8Array {
   const publicKey = publicKeyFromPem(key);
+  //forge only accept string to encrypt, this is to transform Uint8Array to string.
+  // don't use data.toString
+  // must use Array.prototype.map, because Uint8Array's `map` method will force convert element to number,
   const result = Array.prototype.map.call(data, i => String.fromCharCode(i)).join('');
   const cipher = publicKey.encrypt(result, 'RSA-OAEP')
+  // the cipher is binary encoded simply as utf8, so need to convert to binary
   const encryptedData: Uint8Array = cipher.split('').map((item: string) => item.charCodeAt(0));
   return encryptedData
 }
